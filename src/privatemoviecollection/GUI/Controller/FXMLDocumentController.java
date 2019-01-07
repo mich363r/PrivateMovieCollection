f5f992300c68deef5f78376bc71674f947746bdf
Thei1186 <44639852+Thei1186@users.noreply.github.com>
2019-01-07 10:44:21 +0100

    MoveStuff

--- src/privatemoviecollection/GUI/Controller/FXMLDocumentController.java
+++ src/privatemoviecollection/GUI/Controller/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package privatemoviecollection;
+package privatemoviecollection.GUI.Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
