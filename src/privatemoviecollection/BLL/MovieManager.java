e21d12b5274f0423ed70279f3354c5412cf0a6a6
Asvør Rasmussen <asvo0011@easv365.dk>
2019-01-07 12:46:58 +0100

    Update MovieManager.java

--- src/privatemoviecollection/BLL/MovieManager.java
+++ src/privatemoviecollection/BLL/MovieManager.java
@@ -5,11 +5,48 @@
  */
 package privatemoviecollection.BLL;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import privatemoviecollection.BE.Movie;
+import privatemoviecollection.DAL.Database.MovieDbDAO;
+
 /**
  *
  * @author kokus
  */
 public class MovieManager
 {
+    MovieDbDAO mDbDAO;
     
+    public MovieManager() throws IOException
+    {
+        this.mDbDAO = new MovieDbDAO();
+    }
+    
+    public void addMovie (Movie movieToAdd) throws SQLException
+    {
+        mDbDAO.addMovie(movieToAdd);
+    }
+    
+    public void deleteMovie (Movie movieToDelete) throws SQLException
+    {
+        mDbDAO.deleteMovie(movieToDelete);
+    }
+    
+    public List<Movie> getAllMovies () throws SQLException
+    {
+        return mDbDAO.getAllMovies();
+    }
+    
+    public Movie getMovie (Movie movie)
+    {
+        return mDbDAO.getMovie(movie.getId());
+    }
+    
+    public List<Movie> searchMovies (String input)
+    {
+        return mDbDAO.searchMovies(input);
+    }
 }
+
