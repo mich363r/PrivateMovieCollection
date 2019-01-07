5a568e7b26ce66c186538e6e6eb0f067280ff736
michael <mich363r@easv365.dk>
2019-01-07 11:18:14 +0100

    DbConnectionProvider

--- src/privatemoviecollection/DAL/Database/DbConnectionProvider.java
+++ src/privatemoviecollection/DAL/Database/DbConnectionProvider.java
@@ -5,11 +5,36 @@
  */
 package privatemoviecollection.DAL.Database;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
 /**
  *
  * @author kokus
  */
 public class DbConnectionProvider
 {
+    private static final String PROP_FILE = "Database_Settings.txt";
+    private final SQLServerDataSource ds;
+
+    public DbConnectionProvider() throws FileNotFoundException, IOException
+    {
+        Properties databaseProperties = new Properties();
+        databaseProperties.load(new FileInputStream(PROP_FILE));
+        ds = new SQLServerDataSource();
+        ds.setServerName(databaseProperties.getProperty("Server"));
+        ds.setDatabaseName(databaseProperties.getProperty("Database"));
+        ds.setUser(databaseProperties.getProperty("User"));
+        ds.setPassword(databaseProperties.getProperty("Password"));
+    }
+
     
+    public Connection getConnection() throws SQLServerException
+    {
+        return ds.getConnection();
+    }
 }
