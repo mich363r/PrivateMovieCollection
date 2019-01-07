cc6bc527edf64898490c623648dea1ef94a1e40e
Asvør Rasmussen <asvo0011@easv365.dk>
2019-01-07 12:34:22 +0100

    MovieDbDao

--- src/privatemoviecollection/BE/Movie.java
+++ src/privatemoviecollection/BE/Movie.java
@@ -17,17 +17,41 @@ public class Movie
     private String location;
 //    private List <Category> category;
     private int id;
+    private double imdbRating;
+    private double personalRating;
 
-    public Movie(String title, String location, int id)
+    public Movie(int id, String title, String location, double imdbRating)
     {
+        this.id = id;
         this.title = title;
         this.location = location;
-        this.id = id;
+        this.imdbRating = imdbRating;
+        
     }
 
     public String getTitle()
     {
         return title;
+    }
+
+    public double getImdbRating()
+    {
+        return imdbRating;
+    }
+
+    public void setImdbRating(double imdbRating)
+    {
+        this.imdbRating = imdbRating;
+    }
+
+    public double getPersonalRating()
+    {
+        return personalRating;
+    }
+
+    public void setPersonalRating(double personalRating)
+    {
+        this.personalRating = personalRating;
     }
 
     public void setTitle(String title)
---
+++ src/privatemoviecollection/DAL/Database/MovieDbDAO.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package privatemoviecollection.DAL.Database;
+
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import privatemoviecollection.BE.Movie;
+
+/**
+ *
+ * @author Asvør
+ */
+public class MovieDbDAO
+{
+    DbConnectionProvider ds;
+
+    public MovieDbDAO() throws IOException
+    {
+        ds = new DbConnectionProvider();
+    }
+    
+public void addMovie(Movie movieToAdd) throws SQLServerException, SQLException
+{
+    String title = movieToAdd.getTitle();
+    String location = movieToAdd.getLocation();
+    
+    try (Connection con = ds.getConnection())
+    {
+        String sql = "INSERT INTO Movie VALUES (?, ?)";
+        PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        pstmt.setString(1, title);
+        pstmt.setString(2, location);
+        pstmt.execute();
+    }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+}
+    
+public void deleteMovie(Movie movieToDelete) throws SQLServerException, SQLException   
+{
+    int id = movieToDelete.getId();
+    
+    try (Connection con = ds.getConnection())
+    {
+        PreparedStatement pstmt = con.prepareStatement("DELETE FROM Movie WHERE id = (?)");
+        pstmt.setInt(1, id);
+        pstmt.execute();
+    }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+}
+
+public List<Movie> getAllMovies () throws SQLServerException, SQLException
+{
+    List<Movie> movieList = new ArrayList<>();
+    
+    try (Connection con = ds.getConnection())
+    {
+        
+        Statement statement = con.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT FROM Movie");
+        while (rs.next())
+        {
+            int id = rs.getInt("id");
+            String title = rs.getString("title");
+            String location = rs.getString("location");
+            double imdbRating = rs.getDouble("imdbRating");
+            
+            movieList.add(new Movie (id, title, location, imdbRating));
+        }
+    }
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+    
+    return movieList;
+}
+
+public Movie getMovie(int id)
+{
+    Movie wantedMovie = null;
+    
+    try (Connection con = ds.getConnection())
+    {
+        PreparedStatement pstmt = con.prepareStatement("SELECT * FROM Movie WHERE id = (?)");
+        pstmt.setInt(1, id);
+        
+        ResultSet rs = pstmt.executeQuery();
+        
+        while (rs.next())
+        {
+            id = rs.getInt("id");
+            String title = rs.getString("title");
+            String location = rs.getString("location");
+            double imdbRating = rs.getDouble("imdbRating");
+            wantedMovie = new Movie(id, title, location, imdbRating);
+            
+        }
+    } 
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+    
+    return wantedMovie;
+}
+
+public List<Movie> searchMovies (String input)
+{
+    List <Movie> movieList = new ArrayList <>();
+    try (Connection con = ds.getConnection())
+    {
+        PreparedStatement pstmt = con.prepareStatement("SELECT * FROM Movie WHERE titlt like ? or category like ?");
+        pstmt.setString(1, "%" + input + "%");
+        pstmt.setString(2, "%" + input + "%");
+        
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next())
+        {
+            int id = rs.getInt("id");
+            String title = rs.getString("title");
+            String location = rs.getString("location");
+            double imdbRating = rs.getDouble("imdbRating");
+//            String category = rs.getString("category");
+
+            movieList.add(new Movie (id, title, location, imdbRating));
+        
+        }
+    } 
+    catch (Exception e)
+    {
+        e.printStackTrace();
+    }
+    return movieList;
+}
+}
