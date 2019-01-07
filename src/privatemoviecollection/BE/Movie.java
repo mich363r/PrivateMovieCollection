f6ae9c5848f9bafca49bd9faac7b0d8e067ad36d
Asvør Rasmussen <asvo0011@easv365.dk>
2019-01-07 11:06:25 +0100

    movie BE

--- src/privatemoviecollection/BE/Movie.java
+++ src/privatemoviecollection/BE/Movie.java
@@ -5,11 +5,48 @@
  */
 package privatemoviecollection.BE;
 
+import java.util.List;
+
 /**
  *
  * @author kokus
  */
 public class Movie
 {
-    
+    private String title;
+    private String location;
+//    private List <Category> category;
+    private int id;
+
+    public Movie(String title, String location, int id)
+    {
+        this.title = title;
+        this.location = location;
+        this.id = id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public void setLocation(String location)
+    {
+        this.location = location;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
 }
