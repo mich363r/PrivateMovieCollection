168ffa21a4f4b09a11e55454a7e8462e6ba00971
Asvør Rasmussen <asvo0011@easv365.dk>
2019-01-08 09:32:32 +0100

    FileChooser working

--- src/privatemoviecollection/GUI/Controller/FXMLDocumentController.java
+++ src/privatemoviecollection/GUI/Controller/FXMLDocumentController.java
@@ -5,6 +5,7 @@
  */
 package privatemoviecollection.GUI.Controller;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -12,14 +13,27 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import org.blinkenlights.jid3.ID3Exception;
+import org.blinkenlights.jid3.ID3Tag;
+import org.blinkenlights.jid3.MP3File;
+import org.blinkenlights.jid3.v1.ID3V1Tag;
+import org.blinkenlights.jid3.v1.ID3V1_0Tag;
+import org.blinkenlights.jid3.v1.ID3V1_1Tag;
+import org.blinkenlights.jid3.v2.ID3V2_3_0Tag;
 
 /**
  * FXML Controller class
  *
- * @author Kokushi
+ * @author Asvør
  */
-public class FXMLDocumentController implements Initializable {
+public class FXMLDocumentController implements Initializable
+{
 
+    @FXML
+    private AnchorPane rootPane2;
     @FXML
     private TableView<?> tbViewMovie;
     @FXML
@@ -31,12 +45,52 @@ public class FXMLDocumentController implements Initializable {
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb)
+    {
         // TODO
     }    
 
+
     @FXML
-    private void handleButtonAction(ActionEvent event) {
+    private void chooseFile(ActionEvent event)
+    {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Select a file (mp4)", "*.mp4");
+        fileChooser.getExtensionFilters().add(filter);
+        fileChooser.setTitle("Open Music File");
+        Stage stage = (Stage) rootPane2.getScene().getWindow();
+        File mediafile = fileChooser.showOpenDialog(stage);
+        String title = null;
+        String location = null;
+
+        MP3File mp3 = new MP3File(mediafile);
+        try
+        {
+            for (ID3Tag tag : mp3.getTags())
+            {
+                if (tag instanceof ID3V1_0Tag || tag instanceof ID3V1_1Tag)
+                {
+                    ID3V1Tag id3Tag = (ID3V1Tag) tag;
+                    title = id3Tag.getTitle();
+                    location = mediafile.getPath();
+                } else if (tag instanceof ID3V2_3_0Tag)
+                {
+                    ID3V2_3_0Tag id3Tag = (ID3V2_3_0Tag) tag;
+                    title = id3Tag.getTitle();
+                    location = mediafile.getPath();
+
+                }
+            }
+           
+        } catch (ID3Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleSearch(ActionEvent event)
+    {
     }
     
 }
