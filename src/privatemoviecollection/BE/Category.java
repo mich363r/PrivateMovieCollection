a8bb652019833f07371b29905c604949942ca204
Asvør Rasmussen <asvo0011@easv365.dk>
2019-01-08 11:34:14 +0100

    Update Category.java

--- src/privatemoviecollection/BE/Category.java
+++ src/privatemoviecollection/BE/Category.java
@@ -13,6 +13,12 @@ public class Category
 {
     int id;
     String name;
+    
+    public Category(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
 
     public int getId()
     {
