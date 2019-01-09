9dcbc839105190dc348a10f7fbbd5a35e3915a09
Asvør Rasmussen <asvo0011@easv365.dk>
2019-01-09 12:14:13 +0100

    oprydning

--- src/privatemoviecollection/GUI/Controller/AddMovieWindowController.java
+++ src/privatemoviecollection/GUI/Controller/AddMovieWindowController.java
@@ -103,61 +103,22 @@ public class AddMovieWindowController implements Initializable
         } catch (ID3Exception e)
         {
             e.printStackTrace();
-        }
-        
-        
+        }  
     }
-    
-//    Stage primeStage = (Stage) btnSaveSong.getScene().getWindow();
-//
-//        String title = this.txtTitle.getText();
-//        String artist = this.txtArtist.getText();
-//        String location = this.txtFile.getText();
-//
-//        Song newSong = new Song(title, artist, location, 0);
-//        tModel.addSong(newSong);
-//
-//        primeStage.close();
-    
-    private void chooseFile(ActionEvent event)
-    {
-        FileChooser fileChooser = new FileChooser();
-        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Select a file (mp4)", "*.mp4");
-        fileChooser.getExtensionFilters().add(filter);
-        fileChooser.setTitle("Open Music File");
-        Stage stage = (Stage) rootPane2.getScene().getWindow();
-        File mediafile = fileChooser.showOpenDialog(stage);
-        String title = null;
-        String location = null;
 
-        MP3File mp3 = new MP3File(mediafile);
-        try
-        {
-            for (ID3Tag tag : mp3.getTags())
-            {
-                if (tag instanceof ID3V1_0Tag || tag instanceof ID3V1_1Tag)
-                {
-                    ID3V1Tag id3Tag = (ID3V1Tag) tag;
-                    title = id3Tag.getTitle();
-                    location = mediafile.getPath();
-                } else if (tag instanceof ID3V2_3_0Tag)
-                {
-                    ID3V2_3_0Tag id3Tag = (ID3V2_3_0Tag) tag;
-                    title = id3Tag.getTitle();
-                    location = mediafile.getPath();
-
-                }
-            }
-           
-        } catch (ID3Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-    
     public void setModel (MovieModel mModel)
     {
         this.mModel = mModel;
+    }
+
+    @FXML
+    private void saveMovie(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void cancelMovie(ActionEvent event)
+    {
     }
     
     }
